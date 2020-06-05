b78260da84916f2577ba7ba874c4cb4187148f8b
chaehyeon01 <cougs633@naver.com>
2020-06-05 20:43:17 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.3fb9bb1e82ef0a89c8897994304c2d41.js"
+  "/movie_app/precache-manifest.b8da5b975b1dea7cdb6aecb72d3f141a.js"
 );
 
 self.addEventListener('message', (event) => {
